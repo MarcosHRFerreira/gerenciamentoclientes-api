250ef82582481a7383b87eb0eaf71025ca90fb3f
MarcosHRFerreira <marcoshrferreira@gmail.com>
2024-10-31 16:19:05 -0300

    carga inicial

---
+++ src/main/java/br/com/fiap/tc/gerenciamentoclientes_api/GerenciamentoclientesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tc.gerenciamentoclientes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoclientesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoclientesApiApplication.class, args);
+	}
+
+}
